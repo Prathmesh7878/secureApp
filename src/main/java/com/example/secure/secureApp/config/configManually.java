63e9c919b125d89dc19896633fda46fed78c36d7
Prathmesh7878 <154068934+Prathmesh7878@users.noreply.github.com>
2026-03-31 01:36:06 +0530

    Disabled csrf using lamda function

--- src/main/java/com/example/secure/secureApp/config/configManually.java
+++ src/main/java/com/example/secure/secureApp/config/configManually.java
@@ -1,9 +1,12 @@
 package com.example.secure.secureApp.config;
 
+import org.apache.coyote.Request;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +15,10 @@ public class configManually {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(Customizer -> Customizer.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
     }
 }
