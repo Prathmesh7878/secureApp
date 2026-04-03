7b559beef9de4d2363dfc5709ea5042d3687a978
Prathmesh7878 <154068934+Prathmesh7878@users.noreply.github.com>
2026-04-03 14:38:58 +0530

    implemented BCrypt for storing user with encrpyted password in DB

--- src/main/java/com/example/secure/secureApp/service/registerService.java
+++ src/main/java/com/example/secure/secureApp/service/registerService.java
@@ -4,14 +4,18 @@ import com.example.secure.secureApp.model.user;
 import com.example.secure.secureApp.repo.userRepo;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class registerService {
     @Autowired
     private userRepo repo;
+    private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2A,12);
 
     public user addUser(user user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        System.out.println(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 
